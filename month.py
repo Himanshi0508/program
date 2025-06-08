fa70b16b6ab547f8fd5da750c5ce649663bbc1b9
Himanshi0508 <himanshibarfa05@gmail.com>
2025-06-08 23:06:33 +0530

    month.py

---
+++ month.py
@@ -0,0 +1,27 @@
+month=int(input("enter the number"))
+if(month==1):
+    print("jan")
+elif(month==2):
+    print("feb")     
+elif(month==3):
+    print("mar")
+elif(month==4):
+    print("apr")        
+elif(month==5):
+    print("may")
+elif(month==6):
+    print("jun")
+elif(month==7):
+    print("jul")    
+elif(month==8):
+    print("aug")
+elif(month==9):
+    print("sep")  
+elif(month==10):
+    print("oct")
+elif(month==11):
+    print("nov")          
+elif(month==12):
+    print("dec")
+else:
+    print("nothing")              
