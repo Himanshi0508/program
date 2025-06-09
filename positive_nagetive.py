4b51fd21ce20021ea21fff78b4c0080b7d3977c7
Himanshi0508 <himanshibarfa05@gmail.com>
2025-06-09 23:41:24 +0530

    positive_nagetive.py

---
+++ positive_nagetive.py
@@ -0,0 +1,7 @@
+number=int(input("enter the number"))
+if(number>=0):
+    print("number is positive",number)
+elif(number<0):
+    print("number is nagetive",number)
+else:
+    print("number is zero",number)      
