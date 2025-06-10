6a9024b1037eb369028ef156df4de1502d0ff41a
Himanshi0508 <himanshibarfa05@gmail.com>
2025-06-10 21:56:51 +0530

    first commit

---
+++ tringle.py
@@ -0,0 +1,7 @@
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+c=int(input("enter the number"))
+if(a==b and b==c and c ==a):
+    print("tringle is valid")
+else:
+    print("tringle is invalid")    
